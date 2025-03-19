b8f5ec39189e78f188bf1baa5280a1d6053d73da
PrevailUzodinma <prevailuzodinma@gmail.com>
2025-03-19 17:32:14 +0000

    added the update route to edit an expense detail

--- backend/routes/expense.routes.js
+++ backend/routes/expense.routes.js
@@ -5,5 +5,7 @@ const expenseController = require('../controllers/expense.controllers');
 
 router.post('/', validateExpense, expenseController.addExpense);
 router.get('/', expenseController.getExpenses);
+router.put('/:id', expenseController.updateExpense);
+
 
 module.exports = router;
