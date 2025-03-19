524ec658ae47d54819585fe6e05e73d5288d22d2
PrevailUzodinma <prevailuzodinma@gmail.com>
2025-03-19 16:59:47 +0000

    set up the view expense api route

--- backend/routes/expense.routes.js
+++ backend/routes/expense.routes.js
@@ -4,5 +4,6 @@ const validateExpense = require('../middleware/validateExpense');
 const expenseController = require('../controllers/expense.controllers');
 
 router.post('/', validateExpense, expenseController.addExpense);
+router.get('/', expenseController.getExpenses);
 
 module.exports = router;
