1c69dd9a5cf6d82567326937f9205601ba508ee3
PrevailUzodinma <prevailuzodinma@gmail.com>
2025-03-29 13:29:47 +0000

    updating db_url to use the cloud

--- backend/config/connectDB.js
+++ backend/config/connectDB.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
-const db_uri = process.env.DB_URI; // Local connection
+const db_uri = process.env.DB_URI; 
 
 let connectDB = async () => {
   await mongoose
