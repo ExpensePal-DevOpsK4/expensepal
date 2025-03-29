ac56f499d1195b0878b5aefede40fefb0e40b821
PrevailUzodinma <prevailuzodinma@gmail.com>
2025-03-29 14:36:32 +0000

    set up the cloud db, tested it, removed payment method validation

--- backend/config/connectDB.js
+++ backend/config/connectDB.js
@@ -5,8 +5,8 @@ const db_uri = process.env.DB_URI;
 let connectDB = async () => {
   await mongoose
     .connect(db_uri)
-    .then(() => console.log("Connected to expensepalDB locally"))
-    .catch((err) => console.error("Error connecting to local MongoDB:", err));
+    .then(() => console.log("Connected to expensepalDB"))
+    .catch((err) => console.error("Error connecting to MongoDB:", err));
 };
 
 // Export this functionality
--- backend/validation/expenseValidator.js
+++ backend/validation/expenseValidator.js
@@ -4,7 +4,6 @@ const expenseSchema = Joi.object({
   amount: Joi.number().required(),
   category: Joi.string().required(),
   description: Joi.string().optional(),
-  paymentMethod: Joi.string().required(),
   date: Joi.date().optional(),
 });
 
@@ -12,7 +11,6 @@ const updateExpenseSchema = Joi.object({
     amount: Joi.number().positive(),
     category: Joi.string(),
     description: Joi.string(),
-    paymentMethod: Joi.string(),
     date: Joi.date(),
 }).min(1);
 
