9db4faef269f0d08b2dfc13883feb057e7887fc1
PrevailUzodinma <prevailuzodinma@gmail.com>
2025-05-01 17:29:14 +0100

    debug: pipeline break at test

--- backend/tests/expenses.test.js
+++ backend/tests/expenses.test.js
@@ -1,12 +1,26 @@
-// tests/expenses.test.js
-require("dotenv").config({ path: ".env.test" }); 
+require("dotenv").config({ path: ".env.test" });
 const request = require("supertest");
+const mongoose = require("mongoose");
 const app = require("../app");
 
+let server;
+
+beforeAll(async () => {
+  server = app.listen(process.env.PORT);
+  await mongoose
+    .connect(process.env.DB_URI)
+    .then(() => console.log("Connected to expensepalDB"))
+    .catch((err) => console.error("Error connecting to MongoDB:", err));
+});
+
+afterAll(async () => {
+  await mongoose.connection.close();
+  server.close();
+});
 
 describe("Expenses API", () => {
   it("should return a list of expenses", async () => {
-    const response = await request(app).get("/api/expenses");
+    const response = await request(server).get("/api/expenses");
 
     expect(response.statusCode).toBe(200);
     expect(Array.isArray(response.body.data)).toBe(true);
@@ -19,9 +33,10 @@ describe("Expenses API", () => {
       description: "Uber ride",
     };
 
-    const response = await request(app).post("/api/expenses").send(newExpense);
-
-    expect(response.statusCode).toBe(201); // or 200 if that's what your controller returns
+    const response = await request(server)
+      .post("/api/expenses")
+      .send(newExpense);
+    expect(response.statusCode).toBe(201);
     expect(response.body.data.amount).toBe(1000);
   });
 });
