6ea587ee407b7ff7adeaaa75aa925d3664b9f986
PrevailUzodinma <prevailuzodinma@gmail.com>
2025-05-01 03:33:50 +0100

    refactored navbar and restored delete icon

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -4,12 +4,12 @@ import "./components.scss"
 export const Navbar = () => {
   return (
       <nav>
-          <p className='logo'>Logo</p>
+          <p className='logo'>ExpensePal</p>
           <div className='user'>
               <img src="https://static.vecteezy.com/system/resources/previews/002/002/257/non_2x/beautiful-woman-avatar-character-icon-free-vector.jpg"
                   alt="avatar"
               />
-              <p>Username</p>
+              <p>Tracy</p>
           </div>
     </nav>
   )
