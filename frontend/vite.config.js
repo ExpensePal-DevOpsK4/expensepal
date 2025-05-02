ad81ab1ae9bd4450e370f8cd04b380e60e3fe9f8
PrevailUzodinma <prevailuzodinma@gmail.com>
2025-05-02 14:51:54 +0100

    debug: updating vite config to reduce build time, its been hanging

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  logLevel: 'debug',
   build: {
     sourcemap: false,     // disables source maps to reduce I/O
     minify: false,        // skips minification to reduce CPU load
